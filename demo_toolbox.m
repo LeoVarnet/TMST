eff76a5cb52afb4a0298b1cc7ba59544d71c33cc
LeoVarnet <leo.varnet@ens.fr>
2023-08-21 22:47:56 +0200

    add figures demo

--- demo_toolbox.m
+++ demo_toolbox.m
@@ -14,13 +14,13 @@ t = (1:length(S))/fs;
 [AMspec, fc, mf, step] = AMspectrum(S, fs);
 
 figure;
-subplot(3,5,[1 2],'align'); plot(t, S); ylim((max(abs(S))+1)*[-1 +1]); xlim([0 dur]);title('waveform');
-subplot(3,5,[6 7 11 12],'align'); h = pcolor(step.t, fc, 20*log10(step.E'));title('auditory spectrogram');colormap(tmst_inferno());caxis([-80 10]); xlabel('Time (s)'); ylabel('Frequency (Hz)')
+subplot(3,5,[1 2],'align'); plot(t, S); ylim((max(abs(S))+1)*[-1 +1]); xlim([0 dur]);title('waveform');set(gca,'XTick',[]);
+subplot(3,5,[6 7 11 12],'align'); h = pcolor(step.t, fc, 20*log10(step.E'));title('auditory spectrogram');colormap(tmst_inferno());caxis([-80 10]); xlabel('Time (s)'); ylabel('Frequency (Hz)');set(gca,'XTick',[]);
 set(h,'Edgecolor','none');
-subplot(3,5,[3 4],'align'); semilogx(mf, 10*log10(mean(AMspec,2))); xlim(mf([1 end])); title('marginalized AM spectrum'); ylabel('PSD (dB/Hz)');%xlabel('Modulation freq. (Hz)'); 
-subplot(3,5,[8 9 13 14],'align'); h = pcolor(mf, fc, 10*log10(AMspec')); title('AM spectrum'); colormap(tmst_inferno());caxis([-80 10]); xlabel('Modulation freq. (Hz)'); %ylabel('Frequency (Hz)')
+subplot(3,5,[3 4],'align'); semilogx(mf, 10*log10(mean(AMspec,2))); xlim(mf([1 end])); title('marginalized AM spectrum'); ylabel('PSD (dB/Hz)');set(gca,'XTick',[]); %xlabel('Modulation freq. (Hz)'); 
+subplot(3,5,[8 9 13 14],'align'); h = pcolor(mf, fc, 10*log10(AMspec')); title('AM spectrum'); colormap(tmst_inferno());caxis([-80 10]); xlabel('Modulation freq. (Hz)'); set(gca,'YTick',[]); %ylabel('Frequency (Hz)')
 set(h,'Edgecolor','none');set(gca, 'XScale', 'log');
-subplot(3,5,[10 15],'align'); plot(fc,10*log10(mean(1.9*step.gamma_responses.^2,1)),'r'); hold on; plot(fc,10*log10(mean(step.E.^2,1)),'b'); hold on; plot(fc,10*log10(AMspec'*diff(step.mfb)'),'g'); 
+subplot(3,5,[10 15],'align'); plot(fc,10*log10(mean(1.9*step.gamma_responses.^2,1)),'r'); hold on; plot(fc,10*log10(mean(step.E.^2,1)),'b'); hold on; plot(fc,10*log10(AMspec'*diff(step.mfb)'),'g'); set(gca,'XTick',[]); 
 legend({'2*Excitation pattern','True envelope power','Estimated from AM spectrum'}); view([-90 90]); ylabel('dB')
 xlim(fc([1 end]))
 
@@ -38,7 +38,7 @@ set(gca, 'YScale', 'log');
 subplot(5,3,[12 15],'align');
 semilogx(mf, 10*log10(mean(AMspec,2))); hold on
 semilogx(step.scale, 20*log10(nanmean(AMsgram,1))); hold on
-legend({'AM spectrum','from AM scalo.'}); xlim([min(step.scale) max(step.scale)]); view([-90 90]); ylabel('dB/Hz')
+legend({'AM spectrum','from AM scalo.'}); xlim([min(step.scale) max(step.scale)]); view([-90 90]); ylabel('dB/Hz');set(gca,'XTick',[]);
 
 %% AM wavelet
 
