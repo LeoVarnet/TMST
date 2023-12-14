64872c733bc426764129300390dc700faf2c23f9
LeoVarnet <leo.varnet@ens.fr>
2023-12-14 15:23:07 +0100

    remove local

--- Local/Mspectra_dir_amtoolbox.m
+++
@@ -1,7 +0,0 @@
-function basepath = Mspectra_dir_amtoolbox
-% function basepath = Mspectra_dir_amtoolbox
-
-basepath = 'C:\Users\LSP005\Documents\GitHub\amtoolbox-code\';
-if ~strcmp(basepath(end),filesep)
-   basepath = [basepath filesep];
-end
